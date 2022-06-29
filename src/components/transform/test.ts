395c4e18f70149ada2ebcaeb2c4268ce09ca3a93
Lilac Snacc <lilac.snacc@gmail.com>
2022-06-28 23:51:47 -0400

    derpppp

--- src/components/transform/test.ts
+++
@@ -1,8 +0,0 @@
-import { defaultTransform, Transform } from ".";
-
-test('Transform', () => {
-  const entity = { id: 'derp' }
-  Transform.set(entity, defaultTransform)
-
-  expect(Transform.get(entity)).toBe(defaultTransform)
-})
